044561c7a77df4318bb1e40d717922a116c8c8cc
yangsheng8 <344182792@qq.com>
2019-02-11 17:37:01 +0800

    init

---
+++ space/1.php
@@ -0,0 +1,14 @@
+<?php
+namespace beijing;
+header("content-type:text/html;charset=utf-8");
+class  Animals{
+    public  $obj = 'dog';
+    static  $name ='大黄狗';
+}
+function getmsg(){
+    echo '北京海淀';
+}
+
+const  MN = "1.php的名字";
+
+
---
+++ space/2.php
@@ -0,0 +1,10 @@
+<?php
+function getmsg(){
+    echo "上海";
+}
+
+const  MN = "2.php的名字";
+
+include("./1.php");
+
+echo \beijing\MN;
